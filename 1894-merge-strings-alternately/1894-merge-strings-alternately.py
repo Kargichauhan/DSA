a77a3823a23b1f5e79cdb95a5563191115090e53
Kargi Chauhan <125418128+Kargichauhan@users.noreply.github.com>
2025-01-24 15:34:29 -0800

    Time: 30 ms (92.62%), Space: 17.9 MB (5.69%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -25,20 +25,24 @@ class Solution:
 
         '''
 
-        i = 0 
-        j = 0 
+        
 
         res = []
 
-        while i < len(word1) and j < len(word2):
+        min_len = min(len(word1), len(word2))
+
+        '''for i < len(word1) and j < len(word2):
+            res.append(word1[i])
+            res.append(word2[i])'''
+
+        for i in range(min_len):
             res.append(word1[i])
             res.append(word2[i])
 
-            i += 1
-            j += 1 
-
-        res.append(word1[i:])
-        res.append(word2[j:]) 
+        if len(word1) > len(word2):
+            res.append(word1[min_len:])
+        else:
+            res.append(word2[min_len:])
 
         return "".join(res)
         
