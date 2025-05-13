a31159945688cf3335a3cc4d370f06f0f73d6a84
Kargi Chauhan <125418128+Kargichauhan@users.noreply.github.com>
2025-05-12 18:13:22 -0700

    Time: 3 ms (21.61%), Space: 17.8 MB (70.72%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -1,63 +1,49 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
+        '''
+        breakdown:
+        -> s and t string 
+        -> str1 * str2 = str1 + str2
+
+        approach:
+        -> string 
+        -> pointer over str1 and str2 
+        -> most common = output
+
+
+
+        Complexity:
+        o(n) = tc
+        o(n) = sc
+
+        '''
         len1, len2 = len(str1), len(str2)
 
-
         def isDivisor(l):
-            if len1 % l != 0 or len2 % l != 0:
+            if len1 % 1 != 0 or len2 % 1 != 0:
                 return False
 
+            f1, f2 = len1 // l , len2 // l
 
-            
+            base = str1[:l]
 
-            f1, f2 = len1 // l, len2 // l
-            return str1[:l] * f1 == str1 and str1[:l] * f2 == str2
+            return base * f1 == str1 and base * f2 == str2
 
 
-        for l in range(min(len1, len2), 0 , -1):
+        for l in range(min(len1, len2), 0, -1):
             if isDivisor(l):
                 return str1[:l]
 
         return ""
 
 
-        '''        Sketch:
-
-        Input:   str1 = "ABCABC"
-                 str2 = "ABC"
-
-        Output: "ABC"
-
-        Input: str1 = "LEET"
-            str2 = "CODE"
-        Output: ""
-
-        ALgo:
-
-        Brute force:
-
-        substring ABABAB
-
-        ABAB [A] 
-
-        L1 % Length = 0 
-
-
-
         
 
-        TC: o(n + m) . (m.n)
+            
 
-        SC: o(n + m) . (m.n)
-
-
-
-        '''
-
-
+        
 
         
 
 
-
-
+        
