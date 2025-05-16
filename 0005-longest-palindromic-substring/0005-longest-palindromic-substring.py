1f06b636ae4857ed554e99601976510b38ab5b9a
Kargi Chauhan <125418128+Kargichauhan@users.noreply.github.com>
2025-05-15 21:44:25 -0700

    Time: 283 ms (64.52%), Space: 17.9 MB (46.06%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,21 +1,30 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if len(s) <= 1:
-            return s 
+        n = len(s)
+        if n < 2:
+            return s
 
-        max_len = 1
-        max_str = s[0]
+        start, max_len = 0, 1
 
-        for i in range(len(s) - 1):
-            for j in range(i + 1, len(s)):
-                if j - i + 1 > max_len and s[i : j + 1] == s[i: j + 1][::-1]:
-                    max_len = j - i + 1
-                    max_str = s[i : j + 1]
+        for i in range(n):
+            # --- odd-length centers (single character center) ---
+            l, r = i, i
+            while l >= 0 and r < n and s[l] == s[r]:
+                curr_len = r - l + 1
+                if curr_len > max_len:
+                    start, max_len = l, curr_len
+                # move pointers unconditionally
+                l -= 1
+                r += 1
 
+            # --- even-length centers (between i and i+1) ---
+            l, r = i, i + 1
+            while l >= 0 and r < n and s[l] == s[r]:
+                curr_len = r - l + 1
+                if curr_len > max_len:
+                    start, max_len = l, curr_len
+                l -= 1
+                r += 1
 
-        return max_str
-
-
-
-
-        
+        # one final slice
+        return s[start:start + max_len]
