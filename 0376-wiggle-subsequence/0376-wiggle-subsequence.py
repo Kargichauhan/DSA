721eafb6ff96acab8e15896e62fa6b52f0f49492
Kargi Chauhan <125418128+Kargichauhan@users.noreply.github.com>
2025-05-29 22:22:56 -0700

    Time: 77 ms (13.69%), Space: 17.7 MB (90.33%) - LeetHub

---
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.py
@@ -0,0 +1,18 @@
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        
+        n = len(nums); pos = [1] * n; neg = [1] * n 
+
+
+
+        for i in range(1, n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    pos[i] = max(pos[i], neg[j] + 1)
+
+                elif nums[j] > nums[i]:
+                    neg[i] = max(neg[i], pos[j] + 1)
+
+
+        return max(max(pos), max(neg))
+        
