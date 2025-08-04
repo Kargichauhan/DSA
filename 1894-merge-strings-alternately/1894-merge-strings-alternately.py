6f546647f5babf351a05000d075b5051ecf67bd2
Kargi Chauhan <125418128+Kargichauhan@users.noreply.github.com>
2025-08-04 12:12:11 -0700

    Time: 33 ms (84.74%), Space: 17.6 MB (73.87%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -1,49 +1,20 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        '''
-        breakdown:
+        m = len(word1)
+        n = len(word2)
+        i = 0 
+        j = 0
+        res = [] 
 
-        -> merge the string by adding letters
-        -> letters in the alternating order 
-        -> if the len of word is > other other then add 
-        that greater word len to the end of merged string
+        while i < m or j < n:
+            if i < m:
+                res += word1[i]
+                i+=1
 
-        Approach:
+            if j < n:
+                res += word2[j]
+                j += 1
 
-        -> two pointer
-        -> sliding window 
-
-        word 1 (i) -> pointer will go over each index of word 1
-        word 2 (j) -> pointer will go over each index of word 2
-
-        res = [] -> output alternative combination
-
-        edge case: lower case (case sensitive) + no words ?
-
-        Complexity
-
-        o(n + m) -> time complexity
-        o(1) -> SC
-
-        '''
-
-        i, j = 0, 0 
-        res = []
-
-        while i  < len(word1) and j < len(word2): # INBOUND 
-            res.append(word1[i]) # OUTPUT -> APPEND
-            res.append(word2[j])
-
-            i += 1 # increment 
-            j += 1
-
-        res.append(word1[i:]) # IF WORDS NOT SAME LEN -> COMBINE THE REMANING
-        res.append(word2[j:])
-
-        return "".join(res) #JOIN THE RES
-
-
-        
-
+        return "".join(res)
         
 
