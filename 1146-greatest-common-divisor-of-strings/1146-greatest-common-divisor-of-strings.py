e4a995abb55fb843ae9608929712714382998769
Kargi Chauhan <125418128+Kargichauhan@users.noreply.github.com>
2025-08-04 23:32:35 -0700

    Time: 3 ms (23.59%), Space: 17.8 MB (70.83%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -1,40 +1,19 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        '''
-        breakdown:
-        -> s and t string 
-        -> str1 * str2 = str1 + str2
-
-        approach:
-        -> string 
-        -> pointer over str1 and str2 
-        -> most common = output
-
-
-
-        Complexity:
-        o(n) = tc
-        o(n) = sc
-
-        '''
         len1, len2 = len(str1), len(str2)
-
-        def isDivisor(l):
-            if len1 % 1 != 0 or len2 % 1 != 0:
+        
+        def valid(k):
+            if len1 % k or len2 % k: 
                 return False
-
-            f1, f2 = len1 // l , len2 // l
-
-            base = str1[:l]
-
-            return base * f1 == str1 and base * f2 == str2
-
-
-        for l in range(min(len1, len2), 0, -1):
-            if isDivisor(l):
-                return str1[:l]
-
+            n1, n2 = len1 // k, len2 // k
+            base = str1[:k]
+            return str1 == n1 * base and str2 == n2 * base 
+        
+        for i in range(min(len1, len2), 0, -1):
+            if valid(i):
+                return str1[:i]
         return ""
+        
 
 
         
