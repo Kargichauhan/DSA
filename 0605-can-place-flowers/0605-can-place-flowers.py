6fafcb2a9b7e3a51c9c2ba3b284902e0c3856160
Kargi Chauhan <125418128+Kargichauhan@users.noreply.github.com>
2025-08-05 15:17:18 -0700

    Time: 11 ms (39.55%), Space: 18 MB (82.39%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,45 +1,13 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        '''
-        breakdown:
-        -> flowerbed (plants)
-        -> cannot be planted in adjacent plots
-
-        0's = empty
-        1's = non-empty
-
-        Approach:
-        flowerbed in array 
-        greedy 
-
-
-        empty flowerbed 
-
-
-        '''
-
+        count = 0 
         for i in range(len(flowerbed)):
-            # only try to plant in an empty spot
             if flowerbed[i] == 0:
-                empty_left  = (i == 0) or (flowerbed[i-1] == 0)
-                empty_right = (i == len(flowerbed)-1) or (flowerbed[i+1] == 0)
+                empty_l = (i == 0) or flowerbed[i-1] == 0
+                empty_r = (i == len(flowerbed) - 1) or (flowerbed[i+1] == 0)
 
-                if empty_left and empty_right:
-                    flowerbed[i] = 1  # plant here
-                    n -= 1
-                    if n == 0:
-                        return True
-        return n <= 0
+                if empty_l and empty_r:
+                    flowerbed[i] = 1
+                    count += 1
 
-
-
-        
-
-         
-
-
-
-        
-
-
-        
+        return count >= n
